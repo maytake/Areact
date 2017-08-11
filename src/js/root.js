1c932b7e3d035f7ac30a6711457f8f8f2dc81819
LongKing <LongKing@tonydeMacBook-Pro.local>
2017-08-11 16:03:57 +0800

    1

--- src/js/root.js
+++ src/js/root.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {Button} from 'antd';
 import PCIndex from './components/pc_index';
+import PCNewsDetails from './components/pc_news_details';
+import MobileNewsDetails from './components/mobile_news_details';
 import MobileIndex from './components/mobile_index';
+import PCUserCenter from './components/pc_usercenter';
+import MobileUserCenter from './components/mobile_usercenter';
 import 'antd/dist/antd.css';
 import MediaQuery from 'react-responsive';
 export default class Root extends React.Component {
@@ -11,10 +15,18 @@ export default class Root extends React.Component {
 		return (
 			<div>
 				<MediaQuery query='(min-device-width: 1224px)'>
-					<PCIndex/>
+					<Router history={hashHistory}>
+						<Route path="/" component={PCIndex}></Route>
+						<Route path="/details/:uniquekey" component={PCNewsDetails}></Route>
+						<Route path="/usercenter" component={PCUserCenter}></Route>
+					</Router>
 				</MediaQuery>
 				<MediaQuery query='(max-device-width: 1224px)'>
-					<MobileIndex/>
+					<Router history={hashHistory}>
+						<Route path="/" component={MobileIndex}></Route>
+						<Route path="/details/:uniquekey" component={MobileNewsDetails}></Route>
+						<Route path="/usercenter" component={MobileUserCenter}></Route>
+					</Router>
 				</MediaQuery>
 			</div>
 		);
